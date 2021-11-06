f11410cc3a9ccfdbf6f1d1ddd51de2858efdace9
etti5797 <69636543+etti5797@users.noreply.github.com>
2021-11-06 20:26:04 +0200

    Add files via upload

---
+++ ex3.asm
@@ -0,0 +1,26 @@
+.global _start
+
+.section .text
+_start:
+movq a,%rax
+movq b,%rbx
+gcd:
+movq %rbx, %rcx
+xor %rdx,%rdx
+divq %rbx
+movq %rcx,%rax
+movq %rdx,%rbx
+cmp $0,%rbx
+jne gcd
+movq %rax, %rsi 
+lcm:
+movq a,%rax
+movq b,%rbx
+cwd
+imul %rbx
+divq %rsi
+movq %rax,c
+end:
+
+
+
